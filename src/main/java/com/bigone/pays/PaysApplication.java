6f9dd472810ada096be44240f93c57d73867e6af
koo vemu <coolvm9@gmail.com>
2021-05-31 20:15:47 -0700

    Initial commit

---
+++ src/main/java/com/bigone/pays/PaysApplication.java
@@ -0,0 +1,13 @@
+package com.bigone.pays;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaysApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bigone/pays/PaysApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bigone.pays;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaysApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
